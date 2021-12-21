1c1b07394d128baac11f7dab6d50ec37b4bfcde3
saguba01 <supachaiboonying@gmail.com>
2021-12-21 19:31:26 +0700

    Edit vue config

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,2 @@
 module.exports = {
-    pages: {
-      index: {
-        // the source template
-        template: 'public/index.html',
-        // output as dist/index.html
-        filename: 'index.html',
-        // when using title option,
-        // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-        title: 'Vue Test authen',
-        // chunks to include on this page, by default includes
-        // extracted common chunks and vendor chunks.
-        chunks: ['chunk-vendors', 'chunk-common', 'index']
-      },
-    }
-  }
+}
